b68e1ff1ac954c151b358937de2e8f215cc348ca
beardking <barbosa_leandro@rocketmail.com>
2025-08-14 21:10:56 -0300

    sem querer

--- teste.sh
+++
@@ -1,5 +0,0 @@
-curl -i -X POST `
-  https://graph.facebook.com/v22.0/778752671981810/messages `
-  -H 'Authorization: Bearer <access token>' `
-  -H 'Content-Type: application/json' `
-  -d '{ \"messaging_product\": \"whatsapp\", \"to\": \"5519982292047\", \"type\": \"template\", \"template\": { \"name\": \"hello_world\", \"language\": { \"code\": \"en_US\" } } }'
